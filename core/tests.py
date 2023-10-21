67420ad2a85f8eeb92a353df1dd0a3e9da4ca224
NurislamBejsebaev <130899878+NurislamBejsebaev@users.noreply.github.com>
2023-10-21 21:09:13 +0600

    'added_jwt_token'

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,20 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .factories import BookFactory
+import json
+from rest_framework.test import APIClient
+from usersapp.factories import UserFactory
+from rest_framework.authtoken.models import Token
 
 
 class BookAPITestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
 
     def test_create_book(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         data = {
             "title": "Sample Book",
             "author": "John Doe",
@@ -15,7 +24,7 @@ class BookAPITestCase(APITestCase):
             "language": "English",
             "page_count": 300,
         }
-        response = self.client.post('/books/books/', data, format='multipart')
+        response = self.client.post('/books/books/', json.dumps(data), content_type='application/json')
 
         # Распечатайте тело ответа для получения дополнительных сведений об ошибке.
         # print(response.content.decode())
@@ -23,18 +32,28 @@ class BookAPITestCase(APITestCase):
 
         # Добавьте отладочные утверждения для анализа проблемы.
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
     def test_get_book_list(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         BookFactory.create_batch(5)
         response = self.client.get('/books/books/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 5)
 
     def test_get_book_detail(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book = BookFactory()
         response = self.client.get(f'/books/books/{book.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_book(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book = BookFactory()
         data = {
             "title": "Updated Title",
@@ -45,21 +64,27 @@ class BookAPITestCase(APITestCase):
             "language": "Russian",
             "page_count": 301
         }
-        response = self.client.put(f'/books/books/{book.id}/', data, format='json')
+        response = self.client.put(f'/books/books/{book.id}/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["title"], "Updated Title")
         self.assertEqual(response.data["author"], "Updated Author")
 
     def test_partial_update_book(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book = BookFactory()
         data = {
             "title": "Updated Title"
         }
-        response = self.client.patch(f'/books/books/{book.id}/', data, format='json')
+        response = self.client.patch(f'/books/books/{book.id}/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["title"], "Updated Title")
 
     def test_delete_book(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book = BookFactory()
         response = self.client.delete(f'/books/books/{book.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- reader/tests.py
+++ reader/tests.py
@@ -1,49 +1,75 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .factories import BookFactory, ReaderFactory, BookReadingFactory
+import json
+from rest_framework.test import APIClient
+from usersapp.factories import UserFactory
+from rest_framework.authtoken.models import Token
 
 
 class ReaderAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
     def test_create_reader(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         data = {
             "name": "Alice",
             "email": "alice@example.com"
         }
-        response = self.client.post('/readers/readers/', data, format='json')
+        response = self.client.post('/readers/readers/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_reader_list(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         ReaderFactory.create_batch(5)
         response = self.client.get('/readers/readers/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 5)
 
     def test_get_reader_detail(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         reader = ReaderFactory()
         response = self.client.get(f'/readers/readers/{reader.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_reader(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         reader = ReaderFactory()
         data = {
             "name": "Updated Name",
             "email": "updated@example.com"
         }
-        response = self.client.put(f'/readers/readers/{reader.id}/', data, format='json')
+        response = self.client.put(f'/readers/readers/{reader.id}/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], "Updated Name")
         self.assertEqual(response.data["email"], "updated@example.com")
 
     def test_partial_update_reader(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         reader = ReaderFactory()
         data = {
             "name": "Updated Name"
         }
-        response = self.client.patch(f'/readers/readers/{reader.id}/', data, format='json')
+        response = self.client.patch(f'/readers/readers/{reader.id}/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], "Updated Name")
 
     def test_delete_reader(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         reader = ReaderFactory()
         response = self.client.delete(f'/readers/readers/{reader.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -51,6 +77,9 @@ class ReaderAPITestCase(APITestCase):
 
 class BookReadingAPITestCase(APITestCase):
     def test_create_book_reading(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book = BookFactory()
         reader = ReaderFactory()
         data = {
@@ -58,21 +87,30 @@ class BookReadingAPITestCase(APITestCase):
             "reader": reader.id,
             "date_read": "2023-01-15"
         }
-        response = self.client.post('/readers/bookreading/', data, format='json')
+        response = self.client.post('/readers/bookreading/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_book_reading_list(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         BookReadingFactory.create_batch(5)
         response = self.client.get('/readers/bookreading/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 5)
 
     def test_get_book_reading_detail(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book_reading = BookReadingFactory()
         response = self.client.get(f'/readers/bookreading/{book_reading.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_book_reading(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book = BookFactory()
         reader = ReaderFactory()
         book_reading = BookReadingFactory()
@@ -81,21 +119,27 @@ class BookReadingAPITestCase(APITestCase):
             'reader': reader.id,
             "date_read": "2023-01-20"
         }
-        response = self.client.put(f'/readers/bookreading/{book_reading.id}/', data, format='json')
+        response = self.client.put(f'/readers/bookreading/{book_reading.id}/', json.dumps(data), content_type='application/json')
         # print(response.content.decode())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["date_read"], "2023-01-20")
 
     def test_partial_update_book_reading(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book_reading = BookReadingFactory()
         data = {
             "date_read": "2023-01-25"
         }
-        response = self.client.patch(f'/readers/bookreading/{book_reading.id}/', data, format='json')
+        response = self.client.patch(f'/readers/bookreading/{book_reading.id}/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["date_read"], "2023-01-25")
 
     def test_delete_book_reading(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         book_reading = BookReadingFactory()
         response = self.client.delete(f'/readers/bookreading/{book_reading.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
