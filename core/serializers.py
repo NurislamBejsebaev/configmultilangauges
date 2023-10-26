68650a02e3035a0347498470d46d72f0b60c4497
NurislamBejsebaev <130899878+NurislamBejsebaev@users.noreply.github.com>
2023-10-26 21:54:47 +0600

    'update_translations'

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Book
+from django.utils.translation import gettext_lazy as _
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = _('__all__')
